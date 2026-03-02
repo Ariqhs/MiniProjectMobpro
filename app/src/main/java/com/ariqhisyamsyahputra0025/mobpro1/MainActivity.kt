cfcc07ccfb89eb1dfe97af6d7365e745fefb989c
Ariq Hisyam Syahputra <ariqhs5@gmail.com>
2026-03-02 22:41:54 +0700

    Menambahkan Top App Bar

--- app/src/main/java/com/ariqhisyamsyahputra0025/mobpro1/MainActivity.kt
+++ app/src/main/java/com/ariqhisyamsyahputra0025/mobpro1/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.ariqhisyamsyahputra0025.mobpro1
 
+import android.content.pm.ConfigurationInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.ariqhisyamsyahputra0025.mobpro1.ui.theme.Mobpro1Theme
 
@@ -19,29 +26,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mobpro1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Mobpro1Theme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+    @Composable
+    fun MainScreenPreview() {
+        Mobpro1Theme {
+            MainScreen()
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MainScreen() {;
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(id = R.string.app_name))
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    )
+                )
+            }
+        ) { innerPadding ->
+            ScreenContent(Modifier.padding(innerPadding))
+        }
+    }
+
+    @Composable
+    fun ScreenContent(modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello Android!",
+            modifier = modifier
+        )
     }
 }
