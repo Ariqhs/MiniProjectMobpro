0a167351995e68bf3aa5a2eebbe11e9e35a0a81b
Ariq Hisyam Syahputra <ariqhs5@gmai.com>
2026-04-06 10:08:35 +0700

    Fixing things up

---
+++ app/src/main/java/com/ariqhisyamsyahputra0025/mobpro1/ui/screen/MainScreen.kt
@@ -0,0 +1,183 @@
+package com.ariqhisyamsyahputra0025.mobpro1.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Hitung Bangun Datar")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    var panjang by remember { mutableStateOf("") }
+    var lebar by remember { mutableStateOf("") }
+
+    var luas by remember { mutableFloatStateOf(0f) }
+    var keliling by remember { mutableFloatStateOf(0f) }
+
+    var panjangError by remember { mutableStateOf(false) }
+    var lebarError by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = "Mari menghitung luas dan keliling persegi panjang! Masukkan data berikut:",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        OutlinedTextField(
+            value = panjang,
+            onValueChange = { panjang = it },
+            label = { Text(text = "Panjang") },
+            isError = panjangError,
+            trailingIcon = { IconPicker(panjangError, "") },
+            supportingText = { ErrorHint(panjangError) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = lebar,
+            onValueChange = { lebar = it },
+            label = { Text(text = "Lebar") },
+            isError = lebarError,
+            trailingIcon = { IconPicker(lebarError, "") },
+            supportingText = { ErrorHint(lebarError) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {
+                    panjangError = (panjang == "" || panjang == "0")
+                    lebarError = (lebar == "" || lebar == "0")
+
+                    if (panjangError || lebarError) return@Button
+
+                    val p = panjang.toFloat()
+                    val l = lebar.toFloat()
+
+                    luas = p * l
+                    keliling = 2 * (p + l)
+                },
+                modifier = Modifier.padding(end = 12.dp),
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+            ) {
+                Text(text = "Hitung")
+            }
+
+            OutlinedButton(
+                onClick = {
+                    panjang = ""
+                    lebar = ""
+                    luas = 0f
+                    keliling = 0f
+                    panjangError = false
+                    lebarError = false
+                },
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+            ) {
+                Text(text = "Reset")
+            }
+        }
+
+        if (luas != 0f) {
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 16.dp),
+                thickness = 1.dp
+            )
+            Text(
+                text = "Luas: $luas",
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = "Keliling: $keliling",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
+}
+
+@Composable
+fun IconPicker(isError: Boolean, unit: String) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    } else {
+        Text(text = unit)
+    }
+}
+
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = "Input tidak boleh kosong atau 0")
+    }
+}
