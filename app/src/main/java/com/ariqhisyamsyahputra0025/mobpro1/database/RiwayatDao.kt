1eea8cecd5a8a1525ccc3fbcfc692819647ba8b1
Ariq Hisyam Syahputra <ariqhs5@gmai.com>
2026-05-13 00:17:07 +0700

    menambahkan database

---
+++ app/src/main/java/com/ariqhisyamsyahputra0025/mobpro1/database/RiwayatDao.kt
@@ -0,0 +1,22 @@
+package com.ariqhisyamsyahputra0025.mobpro1.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Delete
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RiwayatDao {
+    @Insert
+    suspend fun insertRiwayat(riwayat: RiwayatKonversi)
+
+    @Query("SELECT * FROM tabel_riwayat ORDER BY id DESC")
+    fun getAllRiwayat(): Flow<List<RiwayatKonversi>>
+
+    @Query("DELETE FROM tabel_riwayat")
+    suspend fun deleteAllRiwayat()
+
+    @Delete
+    suspend fun deleteRiwayat(riwayat: RiwayatKonversi)
+}
